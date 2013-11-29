15b5d429f73e0a9194327d226fe76f8ff22b816f
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2013-11-29 13:02:30 +0100

    tests: Add valgrind support
    Add support for running tests under valgrind and reporting
    valgrind-detected errors.
    
    Allow forcing valgrind on arbitrary tests by setting USE_VALGRIND=1.
    
    Add new 'test_write_and_read_with_valgrind' test case, which runs
    the usual 'test_write_and_read' under valgrind.

--- tests/tools/test_main.sh
+++ tests/tools/test_main.sh
@@ -1,10 +1,22 @@
 set -e
 set -u
+
+# Enable alias expansion and clear inherited aliases.
+unalias -a
+shopt -s expand_aliases
+
+command_prefix=
+for i in mfsmaster mfschunkserver mfsmount; do
+	alias $i="\${command_prefix} $i"
+done
+
 . tools/config.sh
 . tools/random.sh
 . tools/system.sh
 . tools/network.sh
 . tools/test.sh
+. tools/valgrind.sh
 . tools/lizardfs.sh
 . tools/timeout.sh
+
 test_begin
